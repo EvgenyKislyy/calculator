8c8bfa551b49939d1cfae0d92e7af27da8aa4307
EugeneKislyy <eugene.kisly@gmail.com>
2018-12-19 01:32:00 +0600

    refactor, console output

--- src/main/java/org/lockdog/calculator/Calculator.java
+++ src/main/java/org/lockdog/calculator/Calculator.java
@@ -20,14 +20,15 @@ public class Calculator {
             //receive the input string, and trying to split into the different arguments
             String[] newInput = inputOutput.readNext().trim().split(" ");
 
-            for (int cnt = 0; cnt < newInput.length; cnt++) {
-                if (!newInput[cnt].isBlank()) {
-                    //lets try to parse the number,
-                    if (!tryToParseTheNumber(newInput[cnt], newInput.length)) {
-                        //  if no - lets try to parse math sign
-                        tryToParseMathSign(newInput[cnt]);
-                        // and if it was math sign - lets do the calculation
-                    }
+            for (String argument : newInput) {
+                if (argument.isBlank()) {
+                    continue;
+                }
+                //lets try to parse the number,
+                if (!tryToParseTheNumber(argument, newInput.length)) {
+                    //  if no - lets try to parse math sign
+                    tryToParseMathSign(argument);
+                    // and if it was math sign - lets do the calculation
                 }
             }
 
@@ -35,9 +36,9 @@ public class Calculator {
         exit();
     }
 
-    private boolean tryToParseMathSign(String userInput) {
-        if (userInput.length() == 1) {
-            Optional<Operation> operation = Operation.fromCharacter(userInput.charAt(0));
+    private boolean tryToParseMathSign(String input) {
+        if (input.length() == 1) {
+            Optional<Operation> operation = Operation.fromCharacter(input.charAt(0));
             if (operation.isPresent()) {
                 //if it's math sign, then do calculation, if not - do nothing, waiting for next user input
 
@@ -64,14 +65,14 @@ public class Calculator {
     /**
      * Try to parse the number from string
      *
-     * @param userInput
+     * @param input
      * @param length    prints the number if only it was the only one input
      * @return
      */
-    private boolean tryToParseTheNumber(String userInput, int length) {
+    private boolean tryToParseTheNumber(String input, int length) {
         try {
             //trying to parse the number
-            Double number = Double.valueOf(userInput);
+            Double number = Double.valueOf(input);
             numbersService.add(number);
             if (length == 1) {
                 inputOutput.print(number.toString());
